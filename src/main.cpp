68728fe78627e71299dbb3733efe03346c2fc4a9
Samuel Whiteley <bling.nuke@gmail.com>
2015-09-18 14:30:54 -0600

    Still figuring gradle cpp build out, build broken

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+
+void main () {
+	int math = mathPlz(1);
+	printf("huehuehuehuehue %d", math);
+}
+
+int mathPlz(int x) {
+	return x * 5 + 3 / 4;
+}
---
+++ src/main.h
@@ -0,0 +1,3 @@
+#pragma once
+#include <cstdio>
+int mathPlz(int x);
