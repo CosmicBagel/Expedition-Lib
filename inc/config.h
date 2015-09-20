4f7cab1e21ba37af1f910acba1b05cd000c407f8
Samuel Whiteley <bling.nuke@gmail.com>
2015-09-20 17:47:05 -0600

    Basic cmake build working

---
+++ inc/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// the configured options and settings for Tutorial 
+// these are set by cmake
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+
+void main () {
+	printf("lolololol %d \n", doSomeMath(5));
+}
+
+int doSomeMath(int x) {
+	return x + 3 * 4 / 2 * 7;
+}
